a65a46345574eed78bf7f01b993d2cc41ba07f65
Thercístenes <thercistenes1@outlook.com>
2023-05-28 10:19:39 -0300

    inicio de estilização prórpria

---
+++ script.js
@@ -0,0 +1,30 @@
+// Validação de dados do botão "Entrar"
+const btnLogin = document.querySelector('.trybewarts-login button');
+const loginEmail = document.getElementById('header-email-input');
+const loginPassword = document.getElementById('header-password-input');
+
+btnLogin.addEventListener('click', () => {
+  if (loginEmail.value === 'tryber@teste.com' && loginPassword.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+const updateCounter = () => {
+  const textArea = document.querySelector('#textarea');
+  const counter = document.querySelector('#counter');
+  const maxLength = 500;
+  const comprimentoLinha = textArea.value.length;
+  const numeroCaracteres = maxLength - comprimentoLinha;
+  counter.textContent = numeroCaracteres;
+};
+
+updateCounter();
+
+const agreement = document.querySelector('#agreement');
+const submitBtn = document.querySelector('#submit-btn');
+
+agreement.addEventListener('change', () => {
+  submitBtn.disabled = !agreement.checked;
+});
